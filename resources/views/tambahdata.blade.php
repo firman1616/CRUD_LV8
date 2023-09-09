aa15c401307c4233a73602f67e93afa3c8d97e8b
firman1616 <firmanfirdaus542@gmail.com>
2023-09-09 15:53:56 +0700

    update template pada tambah data

--- resources/views/tambahdata.blade.php
+++ resources/views/tambahdata.blade.php
@@ -1,74 +1,73 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <title>CRUD LARAVEL</title>
-</head>
-
-<body>
-    <h1 class="text-center mb-4">Tambah Data Pegawai</h1>
-
-    <div class="container">
-
-        <div class="row justify-content-center">
-            <div class="col-8">
-                <div class="card">
-                    <div class="card-body">
-                        <form action="/insertdata" method="POST" enctype="multipart/form-data">
-                            @csrf
-                            <div class="mb-3">
-                                <label for="exampleInputEmail1" class="form-label">Nama Pegawai</label>
-                                <input type="text" class="form-control" id="nama" name="nama">
-                            </div>
-                            <div class="mb-3">
-                                <label for="exampleInputPassword1" class="form-label">Jenis Kelamin</label>
-                                <select name="jenis_kelamin" id="jenis_kelamin" class="form-control">
-                                    <option value="" disabled selected>Choose One</option>
-                                    <option value="pria">Laki - Laki</option>
-                                    <option value="wanita">Wanita</option>
-                                </select>
-                            </div>
-                            <div class="mb-3">
-                                <label for="exampleInputEmail1" class="form-label">No Telp</label>
-                                <input type="number" class="form-control" id="no_telp" name="no_telp">
-                            </div>
-                            <div class="mb-3">
-                                <label for="exampleInputEmail1" class="form-label">Foto Pegawai</label>
-                                <input type="file" class="form-control" id="foto" name="foto">
-                            </div>
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                        </form>
+@extends('layout.admin');
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Tambah Data</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Tambah Data Pegawai</li>
+                        </ol>
                     </div>
                 </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+            <!-- Default box -->
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Tambah Data Pegawai</h3>
+
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                            <i class="fas fa-minus"></i>
+                        </button>
+                        <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                            <i class="fas fa-times"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <form action="/insertdata" method="post" enctype="multipart/form-data">
+                        @csrf
+                        <div class="form-group">
+                            <label for="exampleInputEmail1" class="form-label">Nama Pegawai</label>
+                            <input type="text" class="form-control" id="nama" name="nama">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1" class="form-label">Jenis Kelamin</label>
+                            <select name="jenis_kelamin" id="jenis_kelamin" class="form-control">
+                                <option value="" disabled selected>Choose One</option>
+                                <option value="pria">Laki - Laki</option>
+                                <option value="wanita">Wanita</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1" class="form-label">No Telp</label>
+                            <input type="number" class="form-control" id="no_telp" name="no_telp">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1" class="form-label">Foto Pegawai</label>
+                            <input type="file" class="form-control" id="foto" name="foto">
+                        </div>
+                        <button type="submit" class="btn btn-block btn-primary">Simpan Data</button>
+                    </form>
+                </div>
+                <!-- /.card-body -->
             </div>
-        </div>
+            <!-- /.card -->
+
+        </section>
+        <!-- /.content -->
     </div>
-
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
-        integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
-        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
-    </script>
-    -->
-</body>
-
-</html>
+    <!-- /.content-wrapper -->
+@endsection
